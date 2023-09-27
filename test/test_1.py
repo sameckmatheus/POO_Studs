ce2dc7d5097b6bf5e76a7f27eb36cd95ef24d9d7
sameckmatheus <sameckmatheuspro@gmail.com>
2023-09-27 16:30:15 -0300

    Update: Change Structure Code

--- test/test_1.py
+++ test/test_1.py
@@ -1,17 +1,12 @@
-"""
-Exercício
-
-Os alunos devem incrementar o código com variáveis e métodos, se acharem necessário e, ao final, imprimir na tela todo o histórico do aluno, com todas as notas e médias, inclusive informando se o status do aluno foi: "aprovado por média", "aprovado na final" ou "reprovado". Considerando que a média aritimética para aprovação é 7,0 (sete) e após final 5,0 (cinco).
-
-"""
-
 class DesempenhoAcademico():
 
+  # Método construtor
   def __init__(self, aluno):
     self.nomeAluno = aluno
     self.media = 0
     self.situacao = False
 
+  # Calcula a média
   def calcularMedia(self, nota1, nota2):
     self.media = (nota1 + nota2) / 2
     if (self.media >= 7.00):
@@ -34,3 +29,10 @@ class DesempenhoAcademico():
 desempenho = DesempenhoAcademico('Natasha Pelizari')
 desempenho.calcularMedia(7, 8)
 desempenho.situacaoAluno()
+
+"""
+Exercício
+
+Os alunos devem incrementar o código com variáveis e métodos, se acharem necessário e, ao final, imprimir na tela todo o histórico do aluno, com todas as notas e médias, inclusive informando se o status do aluno foi: "aprovado por média", "aprovado na final" ou "reprovado". Considerando que a média aritimética para aprovação é 7,0 (sete) e após final 5,0 (cinco).
+
+"""
