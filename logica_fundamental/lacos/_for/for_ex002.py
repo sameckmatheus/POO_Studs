16ecac22fc97237938edadb0e28f4cf9030d0668
sameckmatheus <sameckmatheuspro@gmail.com>
2023-10-04 19:13:36 -0300

    Update: Change Scrabble Game

---
+++ logica_fundamental/lacos/_for/for_ex002.py
@@ -0,0 +1,9 @@
+for x in range(1, 5 + 1):
+    for y in range(x):
+        print('*', end=" ")
+    print()
+
+for x in range(5 - 1, 0, -1):
+    for y in range(x):
+        print('*', end=" ")
+    print()
