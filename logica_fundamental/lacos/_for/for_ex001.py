fba79c0b1f8526f373cb1800508360cc5c4b3fdf
sameckmatheus <sameckmatheuspro@gmail.com>
2023-10-10 20:50:55 -0300

    Update: Change New Themes

--- logica_fundamental/lacos/_for/for_ex001.py
+++ logica_fundamental/lacos/_for/for_ex001.py
@@ -1,21 +1,16 @@
-class Game:
-    def __init__(self):
-        self.scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
-                       "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
-                       "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
-                       "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
-                       "x": 8, "z": 10}
+scores = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+          "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+          "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+          "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+          "x": 8, "z": 10}
 
-    def get_score(self, palavra):
-        total_score = 0
-        palavra = palavra.lower()
-        for letra in palavra:
-            if letra in self.scores:
-                total_score += self.scores[letra]
+score = input('Digite um palavra: ')
+total_score = 0
+score = score.lower()
 
-        return total_score
+for letra in score:
+    if letra in scores:
+        total_score += scores[letra]
 
 
-game = Game()
-score = game.get_score(input('Digite um palavra: '))
-print(score)
+print(f'você conseguiu {total_score} pontos.')
