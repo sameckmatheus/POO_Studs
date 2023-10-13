81ca0842b555da13d8d8e26bff34185475babd8f
sameckmatheus <sameckmatheuspro@gmail.com>
2023-10-12 22:26:50 -0300

    Update: Change New Exercices

---
+++ logica_fundamental/arrays/arrays_ex002.py
@@ -0,0 +1,9 @@
+# Crie uma lista vazia e adicione 5 elementos a ela.
+
+lista_coisas = []
+
+for i in range(5):
+    elementos = input('digite algo: ')
+    lista_coisas.append(elementos)
+    
+print(lista_coisas)
